26fec4a21b37099fba3558a3951164a3776b8ee7
while <1657551955@qq.com>
2023-06-30 14:51:41 +0800

    添加项目文件。

---
+++ ModLoad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using hex;
+
+namespace AddMaskCountMod
+{
+
+    [BepInPlugin("while.AddMaskCountMod", "增加屏蔽数量", "1.0.0")]
+    public class ModLoad : BaseUnityPlugin
+    {
+        public static ConfigEntry<int> MaskCountIntConfig;
+        void Start()
+        {
+            MaskCountIntConfig = Config.Bind<int>("config", "MastCountIntConfig", 7, "屏蔽数量");
+            new Harmony("while.AddMaskCountMod").PatchAll();
+        }
+    }
+
+}
---
+++ Patches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using hex;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+namespace AddMaskCountMod
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(ProficiencyBuildMaskUI), "UpdateTxt")]
+        public static class ProficiencyBuildMaskUI_UpdateTxt_Patch
+        {
+            public static void Postfix(Text ___txtNum)
+            {
+                ___txtNum.text = Singleton<ProficiencyManager>.Instance.profMaskMap.Count + "/" + ModLoad.MaskCountIntConfig.Value;
+            }
+        }
+        [HarmonyPatch(typeof(ProficiencyManager), nameof(ProficiencyManager.CheckMask))]
+        public static class ProficiencyManager_CheckMask_Patch
+        {
+            public static bool Prefix(ref bool __result, Dictionary<int, bool> ___profMaskMap)
+            {
+                if (___profMaskMap.Count == ModLoad.MaskCountIntConfig.Value)
+                {
+                    __result=false;
+                }
+                else
+                {
+                    __result=true;
+                }
+                return false;
+            }
+        }
+    }
+}
